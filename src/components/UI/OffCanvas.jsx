af4c68223ac85a834daea63c2d2e0415677f29fa
huutai1612 <tai.huu1612@gmail.com>
2021-11-30 12:40:40 +0700

    Add Responsive Navbar

--- src/components/UI/OffCanvas.jsx
+++ src/components/UI/OffCanvas.jsx
@@ -1,3 +1,34 @@
+import { CSSTransition } from 'react-transition-group';
+import { TiDeleteOutline } from 'react-icons/all';
+import styles from './OffCanvas.module.css';
+
 const OffCanvas = (props) => {
-	return <div></div>;
+	console.log(props.className);
+	return (
+		<CSSTransition
+			in={props.show}
+			timeout={2000}
+			unmountOnExit
+			mountOnEnter
+			classNames={{
+				enter: styles.open,
+				exit: styles.close,
+			}}>
+			<div
+				className={`fixed right-0 top-0 lg:hidden flex-col sm:flex  
+          sm:w-80 h-screen z-10 shadow-xl bg-white
+      `}>
+				<div className='flex justify-around items-center mt-5'>
+					<TiDeleteOutline
+						onClick={props.close}
+						className='text-5xl cursor-pointer hover:text-red-400'
+					/>
+					<h2 className='text-2xl text-red-400 ml-24'>{props.title}</h2>
+				</div>
+				<div className={props.className}>{props.children}</div>
+			</div>
+		</CSSTransition>
+	);
 };
+
+export default OffCanvas;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,16 @@
+import ReactDOM from 'react-dom';
 import { NavLink } from 'react-router-dom';
 import { BiSearchAlt, IoCartOutline, GiHamburgerMenu } from 'react-icons/all';
 import Navigation from './Navigation';
+import { useState } from 'react';
+import OffCanvas from '../UI/OffCanvas';
 
 const Header = (props) => {
+	const [isShowNavigation, setIsShowNavigation] = useState(false);
+
+	const showNavigationHandler = () => setIsShowNavigation(true);
+	const closeNavigationHandler = () => setIsShowNavigation(false);
+
 	return (
 		<header className='container mx-auto'>
 			<nav className='grid gap-4 lg:grid-cols-3 sm:grid-cols-2 h-20 items-center'>
@@ -26,10 +34,21 @@ const Header = (props) => {
 						</span>
 					</div>
 					<GiHamburgerMenu
+						onClick={showNavigationHandler}
 						className={`text-xl cursor-pointer text-gray500 
 							sm:block lg:hidden hover:shadow-lg 
 						hover:text-red-400 transition ease-in-out duration-300`}
 					/>
+					{ReactDOM.createPortal(
+						<OffCanvas
+							title='Menu'
+							className='flex flex-col items-end mr-8 mt-5'
+							show={isShowNavigation}
+							close={closeNavigationHandler}>
+							<Navigation />
+						</OffCanvas>,
+						document.getElementById('offcanvas'),
+					)}
 				</div>
 			</nav>
 		</header>
