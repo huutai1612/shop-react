5504b05196b46e35b7e2cde052d1e374f82bb150
huutai1612 <tai.huu1612@gmail.com>
2021-12-04 13:50:43 +0700

    Fix price In Product paga

--- src/components/product/ProductDescription.jsx
+++ src/components/product/ProductDescription.jsx
@@ -5,7 +5,7 @@ import MainBtn from '../UI/MainBtn';
 import ShopBtn from '../UI/ShopBtn';
 
 const ProductDescription = (props) => {
-	console.log(props);
+	const price = props.price && props.price.toFixed(2).toLocaleString();
 	return (
 		<Fragment>
 			<div className='flex my-2 ml-2 items-center'>
@@ -19,9 +19,7 @@ const ProductDescription = (props) => {
 			</div>
 			<div className='bg-main my-2 py-2 flex justify-around items-center'>
 				<p className='text-2xl'>Price :</p>
-				<h2 className=' text-red-400 text-5xl'>
-					$ {props.price.toFixed(2).toLocaleString()}
-				</h2>
+				<h2 className=' text-red-400 text-5xl'>$ {price}</h2>
 			</div>
 			<div className='my-2'>
 				<p className='text-lg'>{props.description}</p>
