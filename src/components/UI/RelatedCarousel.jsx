6a16f1d97e7ff2bbc2358891bfd50f0a26741b59
huutai1612 <tai.huu1612@gmail.com>
2021-12-11 09:54:41 +0700

    Fix carousel

--- src/components/UI/RelatedCarousel.jsx
+++ src/components/UI/RelatedCarousel.jsx
@@ -21,10 +21,14 @@ const RelatedCarousel = (props) => {
 					<div
 						key={index}
 						className='flex justify-end items-end pb-8 relative'
-						style={{
-							background: `url('${item.image}') no-repeat left 10%`,
-						}}>
+						// style={{background: `url('${item.image}') no-repeat left 10%`,	}}
+					>
 						<div>
+							<img
+								className='object-contain w-4/6'
+								src={item.image}
+								alt={item.title}
+							/>
 							<ShopBtn className='absolute top-1/2 right-4 mt-8 px-4 py-2 text-2xl'>
 								{item.title}
 							</ShopBtn>
